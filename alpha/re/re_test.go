cc81166ce3bacec76b776ec5bb1ca76b426da90b
bombless <bombless@126.com>
2014-07-07 10:32:29 +0800

    Rewrited test

--- alpha/re/re_test.go
+++ alpha/re/re_test.go
@@ -3,27 +3,16 @@ import "testing"
 func Test(t *testing.T){
     var tests = []struct{
         re string
-        want bool
-        inputs []string
+        matches, mismatches []string
     }{
         {
             "Marry|has|a|little|goat",
-            true,
             []string{ "Marry", "has", "a", "little", "goat" },
-        },
-        {
-            "Marry|has|a|little|goat",
-            false,
             []string{ "Nothing", "lasts", "forever" },
         },
         {
             "[_a-zA-Z][_a-zA-Z0-9]*",
-            true,
             []string{ "var", "foo", "base64" },
-        },
-        {
-            "[_a-zA-Z][_a-zA-Z0-9]*",
-            false,
             []string{ "one space", "1mystery" },
         },
     }
@@ -32,12 +21,17 @@ func Test(t *testing.T){
         if err != nil{
             t.Error(err)
         }else{
-            want := c.want
-            for _, input := range c.inputs{
-                got := fa.Test(input)
-                if got != want{
-                    t.Errorf("expect %v=RE/%v/.Test(%v), got %v instead",
-                        want, c.re, input, got)
+            pairs := map[bool][]string{
+                true: c.matches,
+                false: c.mismatches,
+            }
+            for want, inputs := range pairs{
+                for _, input := range inputs{
+                    got := fa.Test(input)
+                    if got != want{
+                        t.Errorf("expect %v=RE/%v/.Test(%v), got %v instead",
+                            want, c.re, input, got)
+                    }
                 }
             }
         }
